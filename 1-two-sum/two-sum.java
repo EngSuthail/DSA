d655b6dc2ff56586433bc8f5d3500c4c128ae80d
Mohamed Suthail <e21395@eng.pdn.ac.lk>
2026-02-07 20:10:59 +0530

    Added the description for the problem

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,3 +1,20 @@
+/*
+Intuition
+I realized i needed to sum each of the elements of the array with each of them to find the target
+
+Approach
+Pick a number from the array(x) which is first loop and iterate through the array to find the other value which is target - x which is the inner loop.
+
+Complexity
+Time complexity:
+O(n^2)
+It has loop inside a loop and in worst case it has to iterate through all of them
+
+Space complexity:
+O(1)
+There is no other data structures used that grow with input sizes. Only few variables used.
+*/
+
 class Solution {
     int a, b;
     public int[] twoSum(int[] nums, int target) {
