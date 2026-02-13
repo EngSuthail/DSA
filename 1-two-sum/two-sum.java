68102b5061b30314f349d8ce25697a83ad0d4af7
Mohamed Suthail <e21395@eng.pdn.ac.lk>
2026-02-13 20:11:41 +0530

    Time: 2 ms (99.17%) | Memory: 47.1 MB (70.58%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,41 +1,17 @@
-/*
-Intuition
-I realized i needed to sum each of the elements of the array with each of them to find the target
-
-Approach
-Pick a number from the array(x) which is first loop and iterate through the array to find the other value which is target - x which is the inner loop.
-
-Complexity
-Time complexity:
-O(n^2)
-It has loop inside a loop and in worst case it has to iterate through all of them
-
-Space complexity:
-O(1)
-There is no other data structures used that grow with input sizes. Only few variables used.
-*/
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     int a, b;
     public int[] twoSum(int[] nums, int target) {
-        
+        Map<Integer, Integer> numMap = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
-            boolean flag = false;
-            int x = nums[i];
-            for(int j = i+1;  j < nums.length; j++){
-                if(nums[j] == target - x){
-                    b = j;
-                    flag = true;
-                    break;
-                }
+            int complement = target - nums[i];
+            if(numMap.containsKey(complement)){
+                return new int[]{numMap.get(complement), i};
             }
-            if(flag){
-                a = i;
-            }
+            numMap.put(nums[i], i);
         }
-        int[] result = new int[2];
-        result [0] = a;
-        result [1] = b;
-        return result;
+        return new int[]{};
     }
 }
