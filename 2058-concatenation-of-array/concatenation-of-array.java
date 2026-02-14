4c16b955edd6ed3ab060ebeedfd31eebd2ed4a57
Mohamed Suthail <e21395@eng.pdn.ac.lk>
2026-02-15 01:54:13 +0530

    Time: 1 ms (95.99%) | Memory: 47.2 MB (54.74%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -4,11 +4,8 @@ class Solution {
         int[] nums_2 = new int[length];
         for(int i = 0 ; i< nums.length; i++){
             nums_2[i] = nums[i];
-        }
-        for(int i = 0 ; i< nums.length; i++){
             nums_2[i + length/2] = nums[i];
         }
         return nums_2;
-        
     }
 }
