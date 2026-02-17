548138f6db1d0b4d714eeeada9769f856e019e79
Mohamed Suthail <e21395@eng.pdn.ac.lk>
2026-02-17 18:50:58 +0530

    Enhance documentation for findMaxConsecutiveOnes method
    Added a detailed comment block explaining the logic, intuition, and complexities of the findMaxConsecutiveOnes method.

--- 485-max-consecutive-ones/max-consecutive-ones.java
+++ 485-max-consecutive-ones/max-consecutive-ones.java
@@ -1,3 +1,13 @@
+/*
+Time: 2 ms (98.21%) | Memory: 52.5 MB (84.56%) - LeetSync
+Intuition - I need to collect the number of 1's until 0. And have to return the maximum number of 1's occuring.
+Approach - I store the number of 1s until a 0, and when a zero appears i store it in a variable and made it to zero. Each time i compared the stored value with the
+times, if times is bigger than the stored value i put the times to stored value. And i got the edge case problem where the last element ending with 1. So I had to 
+compare the values after the for loop as well.
+
+Time complexity - O(n)
+Space complexity - O(1)
+*/
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int times = 0, store = 0;
