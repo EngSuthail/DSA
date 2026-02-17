547313aedf14a74e8d310b516c6c419470410eef
Mohamed Suthail <e21395@eng.pdn.ac.lk>
2026-02-17 18:22:57 +0530

    Document shuffle method with complexity analysis
    Added detailed comments explaining the time and space complexity, intuition, and approach for the shuffle method.

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,3 +1,18 @@
+/*
+Time: 0 ms (100.00%) | Memory: 46.50 MB (82.16%) - LeetSync
+Intuition
+I need to put ith element and (i+n)th together
+
+Approach
+Iterated through the given array less then n times and put ith and (i+n)th element together.
+
+Complexity
+Time complexity:
+O(n)
+
+Space complexity:
+O(n)
+*/    
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] arr = new int[2*n];
