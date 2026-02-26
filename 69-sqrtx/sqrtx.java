a42478eb65f2ebf0bc3974609775fdd24e047f36
Mohamed Suthail <e21395@eng.pdn.ac.lk>
2026-02-26 19:49:02 +0530

    Add comments to explain mySqrt implementation

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,3 +1,10 @@
+/*
+Time: 1 ms (97.18%) | Memory: 42.8 MB (41.79%) - LeetSync
+Used binary search to find the square root of the number(since a given number's square root is within 0 to that number)
+used long for low and high to prevent overflow.
+To find the floor value, everytime the square is less than the value, storing it using ans variable. 
+*/
+
 class Solution {
     public int mySqrt(int x) {
         
