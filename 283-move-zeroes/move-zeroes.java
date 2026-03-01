ffb70415efe22cbf0955679b62b844792709d1e1
Mohamed Suthail <e21395@eng.pdn.ac.lk>
2026-03-02 00:13:08 +0530

    Add approaches and complexities

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,3 +1,11 @@
+/*
+Time: 47 ms (5.08%) | Memory: 48.1 MB (5.55%) - LeetSync
+Not an efficient solution. Will try and put an efficient one soon
+finding zero in the array, and then iterating over the balanced array to get the first bigger or smaller value in the array and then swap them together.
+Time complexity - O(n^2)
+Space complexity - O(1)
+*/
+
 class Solution {
     public void moveZeroes(int[] nums) {
         for(int i=0; i< nums.length; i++){
