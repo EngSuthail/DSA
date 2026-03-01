15dc51433ccaae9af4426d08d0abf5e942aa180d
Mohamed Suthail <e21395@eng.pdn.ac.lk>
2026-03-01 21:50:15 +0530

    Add comments for time complexity and approach

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,3 +1,9 @@
+/*
+Time: 17 ms (15.42%) | Memory: 51.9 MB (99.52%) - LeetSync
+put each element as a key in Hash Map. Value is considered as occurences. Each time if it is there in the map, value is increased by 1.
+Time Complexity - O(n)
+Space Complexity - O(n)
+*/
 class Solution {
     public int majorityElement(int[] nums) {
         int ans = 0;
