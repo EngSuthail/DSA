ae77ff57cfc2522b62aa8b4a3afb1cdede18dc35
Mohamed Suthail <e21395@eng.pdn.ac.lk>
2026-03-02 21:55:09 +0530

    Time: 2 ms (90.15%) | Memory: 48.1 MB (5.53%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,22 +1,12 @@
-/*
-Time: 47 ms (5.08%) | Memory: 48.1 MB (5.55%) - LeetSync
-Not an efficient solution. Will try and put an efficient one soon
-finding zero in the array, and then iterating over the balanced array to get the first bigger or smaller value in the array and then swap them together.
-Time complexity - O(n^2)
-Space complexity - O(1)
-*/
-
 class Solution {
     public void moveZeroes(int[] nums) {
-        for(int i=0; i< nums.length; i++){
-            if(nums[i] == 0){
-                for(int j = i + 1; j<nums.length; j++){
-                    if(nums[j] > nums[i] || nums[j] < nums[i]){
-                        swap(nums, i, j);
-                        break;
-                    }
-                }
+        int l = 0, r = 0;
+        for(int i =0; i<nums.length; i++){
+            if(nums[i]!= 0){
+                swap(nums, l,r);
+                l++;
             }
+            r++;
         }
     }
     public static void swap(int[] arr, int i, int j) {
