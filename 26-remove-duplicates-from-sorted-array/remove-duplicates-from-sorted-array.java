e6f60a9989a05c71872aba739f2eb229225bfe3f
Mohamed Suthail <e21395@eng.pdn.ac.lk>
2026-03-02 23:35:30 +0530

    Add the approach and complexities

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,3 +1,14 @@
+/*
+Time: 1 ms (79.24%) | Memory: 47 MB (7.92%) - LeetSync
+Two pointers approach used.
+In starting of the iteration, l and r are pointing to first element.
+Each iteration r is increased. When there are unequal element appear l increased and swapped with r.
+l+1 will give how many elements are uniquely ordered.
+
+Time complexity - O(n)
+Space complexity - O(1)
+*/
+
 class Solution {
     public int removeDuplicates(int[] nums) {
         int l = 0, r = 0;
