c351894eb5dcda7d50e224731a6d267d6ade9bbf
Mohamed Suthail <e21395@eng.pdn.ac.lk>
2026-03-02 21:59:33 +0530

    Update move-zeroes.java with two-pointers method

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,3 +1,13 @@
+/*
+Time: 2 ms (90.15%) | Memory: 48.1 MB (5.53%) - LeetSync
+Found that it can be easily solvable by two pointers.
+Initially, Left and right pointers are pointing to starting of the array.
+In each iteration right pointer moves by one element. If there is a non zero element, it swap with left pointer value, Then left pointer increases by one.
+
+Time complexity - O(n)
+Space complexity - O(1)
+*/
+
 class Solution {
     public void moveZeroes(int[] nums) {
         int l = 0, r = 0;
